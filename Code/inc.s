ca1c8b8b3d4309d4038ec1e65ff2b0ea292cf4f9
Konstantinos <ksakkasuoi@gmail.com>
2020-08-05 21:20:05 +0300

    INCrement

---
+++ Code/inc.s
@@ -0,0 +1,10 @@
+;Οι εντολές INC (INCrement), INCA (INCrement Accumulator), INCX (INCrement indeX register)
+;Σύνταξη
+;INC “ADRRESS”
+;Αυξάνει κατά 1 µονάδα το  περιεχόµενο µιας διεύθυνσης µνήµης
+;(Χ = indeX register)
+;(Α = Accumulator)
+
+INC $0040
+INC COUNTER
+
