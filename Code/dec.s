48af69953c6de8a64f49942ce2c2a566e8bce8fc
Konstantinos <ksakkasuoi@gmail.com>
2020-08-05 21:18:07 +0300

    DECrement

---
+++ Code/dec.s
@@ -0,0 +1,10 @@
+;Οι εντολέςDEC (DECrement), DECA (DECrement Accumulator)  DECX (DECrement indeX register)
+;Σύνταξη
+;DEC “ADRRESS”
+;Μειώνει κατά 1 µονάδα το  περιεχόµενο µιας διεύθυνσης µνήµης 
+;(Χ = indeX register)
+;(Α = Accumulator)
+
+DEC $0040
+DEC COUNTER
+
